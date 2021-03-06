51ed274421622bef0d5f749a63687ebbe1f6577e
Emmy <t_jmeas@yahoo.co.uk>
2021-03-06 16:40:31 +0000

    Update main to use new LCD API, added ttl to readings to stop livelock and made tolerant to bad bytestreams and missing metadata

--- new stuff/main.py
+++ new stuff/main.py
@@ -1,5 +1,5 @@
 from Message import *
-from Log import *
+import Log
 import LCD
 import lcd160cr
 import pyb
@@ -17,20 +17,19 @@ until_len = None  # will be assigned when first packet found
 calibrated = False
 
 gpsIn = UART(6, 38400)
-gpsIn.init(38400, bits=8, parity=None, stop=1, read_buf_len=512, timeout=1500) # timeout should overlap epochs -> 1s atm
+gpsIn.init(38400, bits=8, parity=None, stop=1, read_buf_len=512,
+           timeout=1200)  # timeout should overlap epochs -> 1s atm
 
 BUF_SIZ = 4
 
 # wdt = WDT(timeout=10000)
-TIME_CONF_LIMIT = 24 # number of hours before time-resync
-timeConfidence = 0 # update time on 0 ==> on reset or start time is set
-
-print("Starting...")
-LCD.initLCDAPI()
-PAGE = 0 # used for LCD display
+TIME_CONF_LIMIT = 24 * 60 * 60  # number of readings before time-resync
+timeConfidence = 0  # update time on 0 ==> on reset or start time is set
 
 msg_buf = {}
 msg_count = 0
+
+monitoring = False
 
 ## MUST BE SET FROM 0 -> NO_MSGS ##
 # determines location in each epoch list - don't change!
@@ -38,21 +37,31 @@ LOC_CODE = 0
 STAT_CODE = 1
 SATINF_CODE = 2
 TIMEUTC_CODE = 3
+SVIN_CODE = 4
 
 LOG_RAW = False
 LOG_MEDIAN = True
 LOG_BEST = True
+UPDATE_DELAY = 100
 
-NO_READINGS = 50  # number of positions used in one reading
-NO_MSGS = 4  # number of messages per epoch (HPECEF, SAT, STATUS) = 3
+NO_READINGS = 5  # number of positions used in one reading
+NO_MSGS = 3  # ROVER: number of messages per epoch (HPECEF, SAT, STATUS) = 3 --> NOTE that TIMUTC is used then discarded once time is updated
+MAX_READING_ATTEMPTS = 100 # prevents livelock in case no message triples are valid
+# NO_MSGS = 5 # BASE STATION: number of messages per epoch (HPECEF, SAT, STATUS, TIMEUTC, SVIN) = 5
 # MSG_PERIOD = 8 * 60 * 60  # (in seconds) every eight hours - min. 1 minute (50 readings taken with a delay of 1s
 # between them)
-MSG_PERIOD = 60  # 60-second debug mode
+MSG_PERIOD = 120  # two-minute debug mode
 if MSG_PERIOD < 60:
     MSG_PERIOD = 60
+MSG_PERIOD *= 1000  # convert from s to ms
 
-MSG_START_TIME = 12 # defines the first hour in which the readings will take place. no sub-hour accuracy as intended
+MSG_START_TIME = 12  # defines the first hour in which the readings will take place. no sub-hour accuracy as intended
 # use is for < 10 readings per day
+
+print("Starting...")
+LCD.initLCDAPI(MSG_PERIOD, MSG_START_TIME, LOG_RAW, LOG_MEDIAN, LOG_BEST, False)
+PAGE = 0  # used for LCD display
+
 
 def calibrate():
     print("Calibrating")
@@ -80,11 +89,16 @@ def readBytes():
     class_id = gpsIn.read(2)
     # pack_len_bytes = uart.read(2)
     pack_len_bytes = gpsIn.read(2)
-    pack_len = int.from_bytes(pack_len_bytes, 'little')
+    pack_len = U2(pack_len_bytes)
     # print(pack_len_bytes,pack_len, "bytes long")
-    if pack_len > 100:
-        print(pack_len)
+
+    if pack_len > 100 and class_id != bytearray(b'\x01\x35'):
+        print(pack_len, "invalid length")
         return
+    elif class_id == bytearray(b'\x01\x35'):
+        print("Sat message, length capped")
+        pack_len = 8
+        pack_len_bytes = u2toBytes(pack_len)
     payload = gpsIn.read(pack_len)
     crc = gpsIn.read(2)
     if crc is None:
@@ -97,10 +111,11 @@ def readBytes():
         nextpack.extend(payload)
         nextpack.extend(crc)
         pack_buf.append(nextpack)
-    except:
+    except Exception as e:
+        print(e)
         print("Error:")
         print(class_id)
-        print(pack_len)
+        print(pack_len_bytes)
         print(payload)
         print(crc)
         print("End error")
@@ -189,25 +204,31 @@ def readIn():
 # 2 -> Satellite information (notably the number of satellites used)
 # 3 -> Survey-in data (base station)
 def getMessageFromBuffer():
-    global msg_buf, msg_count, LOC_CODE, STAT_CODE, SATINF_CODE, NO_MSGS
+    global pack_buf, msg_buf, LOC_CODE, STAT_CODE, SATINF_CODE, NO_MSGS
     if len(pack_buf) == 0:
         return None, -1
 
     byte_stream = pack_buf[0]
     del pack_buf[0]
-
+    # print(byte_stream)
     msg = binaryParseUBXMessage(byte_stream)
+    if msg is None:
+        return None, -1
+
     tow = msg.getTOW()
-    print(len(msg_buf))
+    print(len(msg_buf), "messages in buffer")
     if tow not in msg_buf:
         msg_buf[tow] = [None] * NO_MSGS
-
+    code = -1
     if isinstance(msg, HPECEF):
         code = LOC_CODE
     elif isinstance(msg, Status):
         code = STAT_CODE
     elif isinstance(msg, SatInfo):
         code = SATINF_CODE
+    elif isinstance(msg, TimeUTC):
+        print("TIME message incoming")
+        updateTime(msg)
     # SVIN should only come in on base station, leaving code here for ease of copying, could also make code more
     # deployable by copying gps-read code?
     # elif isinstance(msg, SVIN):
@@ -215,73 +236,102 @@ def getMessageFromBuffer():
     else:
         code = -1
 
-    msg_count += 1
-    msg_buf[tow][code] = msg
+    if code != -1:  # just in case msg is not being used -> TIMEUTC? maybe another message has been enabled by accident i.e. LLH
+        msg_buf[tow][code] = msg
     return msg, code
 
 
 reading = False
+
+
 def getReadings(i=0):
     global msg_buf, msg_count, NO_READINGS, STAT_CODE, timeConfidence, MSG_PERIOD, reading
     reading = True
-    LCD.drawBusyPage()
-    while msg_count < NO_READINGS * NO_MSGS:
+    LCD.makeLCDBusy()
+    msg_count = 0
+    msg_buf = {}
+    lastepoch = 0
+    chosen_msgs = []
+    ttl = MAX_READING_ATTEMPTS # time to live, prevents livelock
+    while msg_count < (NO_READINGS + 1) * NO_MSGS + 1 and ttl > 0:
+        ttl -= 1
         readBytes()
         msg, id = getMessageFromBuffer()
-        if id == STAT_CODE:
-            validFix = msg.gpsFixOK
-            if not validFix:
-                msg_count -= 1
-                del msg_buf[msg.iTOW]
+        if msg is None:
+            continue
+        curepoch = msg.getTOW()
+        if curepoch != lastepoch and lastepoch != 0:
+            print("------ ### ------")
+            # if any messages are missing from last epoch, delete data from that epoch as unreliable (incomplete metadata)
+            if any(map(lambda r: r is None, msg_buf[lastepoch])) or len(msg_buf[lastepoch]) < NO_MSGS:
+                msg_count -= sum(map(lambda r: r is not None, msg_buf[lastepoch]))
+                print("Some messages none on turn of next epoch, deleting epoch...")
+                del msg_buf[lastepoch]
+                lastepoch = 0
+            elif LOG_RAW:
+                # safe to log as raw data
+                chosen_msgs.append((b'\xF1', msg_buf))
+        print(msg, id, msg_count)
+        lastepoch = curepoch
+        invalidFix = id == STAT_CODE and not msg.gpsFixOK or id == LOC_CODE and msg.invalidFix
+        if invalidFix:
+            msg_count -= id
+            del msg_buf[msg.iTOW]
+            print("Invalid fix, deleting msg", msg_buf, msg_count, id == STAT_CODE and not msg.gpsFixOK, id == LOC_CODE and msg.invalidFix)
+        else:
+            msg_count += 1
 
-    # clock will drift as time continues, update time when this reaches 0 (see TIME_CONF_LIMIT for hours before reset)
-    timeConfidence -= MSG_PERIOD
-    chosen_msgs = []
+    del msg_buf[curepoch] # trim last epoch from buffer
+    # clock will drift as time continues, update time when this reaches 0 (see TIME_CONF_LIMIT for readings before reset)
+    timeConfidence -= msg_count
     if LOG_MEDIAN:
-        chosen_msgs.append(getMedianMsg(msg_buf))
-        type_code = b'\x01'
+        print(msg_buf, msg_count)
+        type_code = b'\xF1'
+        chosen_msgs.append((type_code, getMedianMsg(msg_buf)))
 
     if LOG_BEST:
         # take message with smallest pAcc --> most accurate of the readings
-        chosen_msgs.append(getBestAcc(msg_buf))
-        type_code = b'\x02'
-
-    if LOG_RAW:
-        # take last message
-        chosen_msgs.append(msg_buf[max(msg_buf)])
-        type_code = b'\x00'
+        type_code = b'\xF2'
+        chosen_msgs.append((type_code, getBestAcc(msg_buf)))
 
     if len(chosen_msgs) > 0:
-        for msg in chosen_msgs:
-            type_code = 0x00
-            location = msg[LOC_CODE]
-            log = BinaryECEFLog()
-            log.tow = location.iTOW  # want to keep the binary here - getTOW() implicitly converts to int
-            log.setECEFData(location)  # works ONLY if we use binaryParseUBXMessage - needs reprogramming to ECEFLog or use
-            # conversion functions
-            log.setSvs(msg[SATINF_CODE])
-            log.setDataType(type_code)
-            writeLog(log)
+        for t, m in chosen_msgs:
+            location = m[LOC_CODE]
+            sats = m[SATINF_CODE]
+            log = Log.ECEFLog(location, t, sats)
+            n = log.writeLog()
+            print(n, "bytes written to file")
+            # log = BinaryECEFLog()
+            # log.tow = location.iTOW  # want to keep the binary here - getTOW() implicitly converts to int
+            # log.setECEFData(location)  # works ONLY if we use binaryParseUBXMessage - needs reprogramming to ECEFLog or use
+            # # conversion functions
+            # log.setSvs(msg[SATINF_CODE])
+            # log.setDataType(type_code)
+            # writeLog(log)
 
     transmitLogs()
     updateLCD()
     updateLEDs()
-    msg_buf = []
+    msg_buf = {}
     msg_count = 0
-    LCD.drawPage()
+    LCD.makeLCDFree()
     reading = False
     # pyb.stop() # put in sleep mode when all done
 
 
 def updateTime(timeMsg):
     global clock, timeConfidence, TIME_CONF_LIMIT
-    if type(timeMsg) is not TimeUTC or timeMsg.validTime() or timeConfidence > 0:
+    if type(timeMsg) is not TimeUTC or not timeMsg.validTime() or timeConfidence > 0:
+        print("No clock update: ", timeConfidence)
+        timeConfidence -= 1
         return
-
+    print("!!! Updating clock !!!")
     timeConfidence = TIME_CONF_LIMIT
     # assume Monday as gps doesn't give this data hence weekday=0
-    clock.datetime(timeMsg.getYear(), timeMsg.getMonth(), timeMsg.getDay(), 1,
-                   timeMsg.getHour(), timeMsg.getMinute(), timeMsg.getSeconds(), timeMsg.getNano())
+    print((timeMsg.getYear(), timeMsg.getMonth(), timeMsg.getDay(), 1,
+           timeMsg.getHour(), timeMsg.getMinute(), timeMsg.getSeconds(), timeMsg.getNano()))
+    clock.datetime((timeMsg.getYear(), timeMsg.getMonth(), timeMsg.getDay(), 1,
+                    timeMsg.getHour(), timeMsg.getMinute(), timeMsg.getSeconds(), timeMsg.getNano()))
 
 
 def writeLog(log):
@@ -347,7 +397,8 @@ def transmitLogs():
 
 # not needed for now
 def updateLCD():
-    LCD.updateLCD(250)
+    global UPDATE_DELAY
+    LCD.updateLCD(UPDATE_DELAY)
 
 
 # might be useful for visual status - green on fix etc.
@@ -414,18 +465,21 @@ def getPos(msg):
 
 
 def getEuclidiean(msgTriple):
-    msg = msgTriple[0]
+    msg = msgTriple[1][0]
+    print(msgTriple, msg)
     px, py, pz = msg.get3DPos()
     return px ** 2 + py ** 2 + pz ** 2
 
 
 # SHOULD return a list of messages with indexes matching the codes
 def getMedianMsg(msgs):
-    sorted_msg_values = {key: value for key, value in
-                         sorted(msgs.items(), key=lambda item: getEuclidiean(item))}.values()
+    print(msgs)
+    sorted_msg_values = list({key: value for key, value in
+                         sorted(msgs.items(), key=lambda item: getEuclidiean(item))})
     halfway_point = (len(sorted_msg_values) + 1) / 2 - 1
     # easier to choose left value rather than average between the two
-    med = sorted_msg_values[int(halfway_point)]
+    print(halfway_point, int(halfway_point), sorted_msg_values)
+    med = msgs[sorted_msg_values[int(halfway_point)]]
     return med
 
 
@@ -448,7 +502,7 @@ def readLoc(i):
             readBytes()
             msg, id = getMessageFromBuffer()
 
-        print(msg.get3DPos(), msg.getPAcc())
+        # print(msg.get3DPos(), msg.getPAcc())
         msgs.append(msg)
 
     # tow, x, y, z, xhp, yhp, zhp, pacc, flags
@@ -487,7 +541,7 @@ def writeMsgToFile(msg, fn):
 # resets clock to use actual period synced up to the time specified
 def intialReading(i=0):
     global MSG_PERIOD, clock
-    getReadings() # take readings immediately
+    getReadings()  # take readings immediately
     clock.wakeup(MSG_PERIOD, getReadings)
 
 
@@ -495,29 +549,51 @@ def intialReading(i=0):
 def initialTimer():
     global MSG_START_TIME, MSG_PERIOD, clock
     if timeConfidence != 0:
-        time=clock.datetime()
+        time = clock.datetime()
         hour = time[4]
-        diff = hour - MSG_START_TIME
+        diff = (hour - MSG_START_TIME) * 60 * 60
         if diff == 0 or diff % MSG_PERIOD == 0:
             clock.wakeup(MSG_PERIOD, initialReading)
+            print("Wakeup in", MSG_PERIOD)
         elif diff < 0:
             clock.wakeup(abs(diff), initialReading)
+            print("Wakeup in", abs(diff))
         elif diff > 0:
             nextLogTime = MSG_START_TIME
             while diff > 0:
                 nextLogTime += MSG_PERIOD
                 diff = hour - nextLogTime
             clock.wakeup(abs(diff), initialReading)
+            print("Wakeup in", abs(diff))
+    else:
+        print("No time confidence")
 
 
 clock = pyb.RTC()
-clock.wakeup(1000, initialTimer()) # start checking each second if time accurate, then start reading properly
+# clock.wakeup(1000, initialTimer()) # start checking each second if time accurate, then start reading properly
 # main loop
+svs = 0
 while True:
-    if not reading: # don't update LCD if taking a reading
+    if not reading:  # don't update LCD if taking a reading
+        monitoring = LCD.monitoring
+        if monitoring:
+            # get a locaiton message from buffer
+            msg_buf = {}  # reset to stop memory leaks - shouldn't interfere with periodic reading as won't be reachable during
+            readBytes()
+            msg, code = getMessageFromBuffer()
+            if code == LOC_CODE:
+                print("Updating location data")
+                LCD.updateLocMonitorData(msg, svs)
+            elif code == SVIN_CODE:
+                print("Updating survey data")
+                LCD.updateSVINMonitorData(msg)
+            elif code == SATINF_CODE:
+                print("SAT DATA INCOMING")
+                svs = msg.getNumSvs()
+        print("Updating LCD")
         updateLCD()
-        pyb.delay(250)
-    pyb.wfi() # put in low-power mode to reduce power consumption
+        pyb.delay(UPDATE_DELAY)
+    pyb.wfi()  # put in low-power mode to reduce power consumption
 
 # while True:
 #     print("---")
