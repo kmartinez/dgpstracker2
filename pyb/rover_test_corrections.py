21365d712049773b21901d994a76d594cdb90069
Sherif Attia <33965805+sua-dev@users.noreply.github.com>
2022-08-02 11:45:59 +0100

    Fully operational page forwarder communicating between both pyboards and gps boards.
    - Will need to do some cleaning up by removing some comments
    - General operation works though. RTCM Messages are sent and written to the gps_uart on the rover via the radio uart. The messages are then parsed using the nmea functions from gps.py.
    
    - Multiple checks are undergone before they are attempted to be send back to the base radio uart. We can finally see some useful information received from the base.

--- pyb/rover_test_corrections.py
+++ pyb/rover_test_corrections.py
@@ -1,10 +1,4 @@
-# Title: rover_test_corrections.py
-# brief: validate that rover can received forwarded messages from base, write them to the gps_uart and then forward nmea
-#        messages back to the base once they have been processed. - Will still test the code out in the field before
-#        confirming it works.
-# author: Sherif Attia (sua2g16)
-#
-
+# Write your code here :-)
 from pyb import UART
 from pyb import RTC
 from gps import *
@@ -29,6 +23,7 @@ gps_uart.init(GPS_BAUDRATE, bits=8, stop=1, read_buf_len=GPS_BUFFER_SIZE)
 
 PACKET_MANAGER = bytearray()
 
+
 print("Pyboard Black - Rover Board")
 # create a poll i.e. wait for incoming messages
 print("Waiting for incoming messages...")
@@ -47,26 +42,57 @@ while True:
 
         print("Reading Messages")
         gps_rover_data = gps_uart.readline()
+        if gps_rover_data is None:
+            continue
+        #             print(gps_rover_data)
+        # need to ignore the contents / skip this message and force the loop to continue
         gps_rover_data = str(gps_rover_data.decode())
+
+        if gps_rover_data.count("$") > 1:
+            continue
         #         print(gps_rover_data)
         #         print(data)
 
         if gpsFormatOutput(ROVER_ID, gps_rover_data) is None:
             continue
-        if gpsFormatOutput(ROVER_ID, gps_rover_data)[0] == 'p':
-            radio_uart.write("3D/DGNSS/FIXED")
+        if gpsFormatOutput(ROVER_ID, gps_rover_data)[0] == "p":
+            radio_uart.write("3D/DGNSS/FIXED\n")
+            fixed_message = gpsFormatOutput(ROVER_ID, gps_rover_data)[1]
+            final_message = (
+                str(fixed_message[0])
+                + ", "
+                + str(fixed_message[1])
+                + ", "
+                + str(fixed_message[2])
+                + ", "
+                + str(fixed_message[3])
+                + ", "
+                + str(fixed_message[4])
+                + ", "
+                + str(fixed_message[5])
+                + ", "
+                + str(fixed_message[6])
+                + ", "
+                + str(fixed_message[7])
+                + "\n"
+            )
+
+            radio_uart.write(final_message)
             # might complain, thus might have to put it in a BUFFER
-            radio_uart.write(gpsFormatOutput(ROVER_ID, gps_rover_data))
+            processed_data = gpsFormatOutput(ROVER_ID, gps_rover_data)
+            print(processed_data)
+        #             PACKET_MANAGER = bytearray(processed_data.decode())
+        #             radio_uart.write(PACKET_MANAGER)
         if gpsFormatOutput(ROVER_ID, gps_rover_data) is not None:
             print(gpsFormatOutput(ROVER_ID, gps_rover_data))
             radio_uart.write("Message Received - Incoming Payload\n")
 
-            if gps_uart.any() > 0:
-                PACKET_MANAGER = bytearray(gps_uart.read())
+    #            if gps_uart.any() > 0:
+    #                 PACKET_MANAGER = bytearray(gps_uart.read())
 
-                radio_uart.write(PACKET_MANAGER)
+    #                 radio_uart.write(PACKET_MANAGER)
 
-        # process incoming nmea messages - split them up and define them separately to get an nmea fix.
+    # process incoming nmea messages - split them up and define them separately to get an nmea fix.
     #         print(data)
     #         print(radio_uart.readline())
     # need to decode incoming byte formatted data
@@ -74,4 +100,3 @@ while True:
 
     # send back data to base.
     pyb.delay(500)
-
