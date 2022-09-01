acf55a77ed33a26ea6342eda25b991d00a69b8f1
ic2n19 <ic2n19@soton.ac.uk>
2022-09-01 18:57:53 +0100

    post to evalkit success

--- SWARM/http_beehive.py
+++ SWARM/http_beehive.py
@@ -11,7 +11,10 @@ login = {"username": "user", "password": "pass"}
 
 headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
+headers_2 = {"Content-Type": "application/json"}
+
 login_url="https://bumblebee.hive.swarm.space/hive/login"
+logout_url="https://bumblebee.hive.swarm.space/hive/logout"
 messages_url ="https://bumblebee.hive.swarm.space/hive/api/v1\
 /messages"
 count_url = "https://bumblebee.hive.swarm.space/hive/api/v1\
@@ -19,9 +22,9 @@ count_url = "https://bumblebee.hive.swarm.space/hive/api/v1\
 
 data_body= {
 "deviceType": 1,
-"deviceId": 8700,
-"userApplicationId": 2317,
-"data": encoded_str
+"deviceId": 3607, #evalkit = 3607, sparkfun m138 = 8700
+"userApplicationId": 1235,
+"data": encoded_str.decode()
 }
 
 session = requests.Session()
@@ -31,12 +34,15 @@ response = session.post(login_url, headers = headers, data = login)
 print(session.cookies.get_dict())
 
 cookies = session.cookies.get_dict()
+
 #print(cookies)
+#response1 = session.get(count_url)#, cookies= cookies)
+#print(response1.json())
 
-response1 = session.get(count_url)#, cookies= cookies)
-
-print(response1.json())
-
-response2 = session.post(messages_url, data = data_body)
+response2 = session.post(messages_url, headers= headers_2, json = data_body)
 
 print(response2.json())
+
+response3 = session.get(logout_url)
+print(response3.text)
+#print(response3.json())
