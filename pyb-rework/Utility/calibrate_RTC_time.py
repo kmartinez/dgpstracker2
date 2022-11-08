6d640021f2697b9be6e29643a6f5b834624ec7ef
slapjd <mwj2g19@soton.ac.uk>
2022-11-08 13:09:55 +0000

    comment

--- pyb-rework/Utility/calibrate_RTC_time.py
+++ pyb-rework/Utility/calibrate_RTC_time.py
@@ -8,4 +8,5 @@ I2C = board.I2C()
 RTC = adafruit_ds3231.DS3231(I2C)
 
 #Set current date and time here
+#(year, month, day, hour, minute, second)
 RTC.datetime = struct_time((2022,11,8,23,59,59,-1,-1,-1))
