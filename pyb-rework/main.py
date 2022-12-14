a217ffb96ef7f9dd1c6ab4ca78c4cc2faa63cb98
christiankuhlmann <99804123+christiankuhlmann@users.noreply.github.com>
2022-12-14 18:28:08 +0000

    log all uncaught exceptions and *always* shutdown

--- pyb-rework/main.py
+++ pyb-rework/main.py
@@ -1,5 +1,7 @@
 from config import *
 import os
+import Device
+import traceback
 
 def debug(
     *values: object,
@@ -8,12 +10,19 @@ def debug(
       print(*values)
 
 if __name__ == "__main__":
-   if "data_entries" not in os.listdir("/"):
-      os.mkdir("/data_entries")
-   #input()
-   if DEVICE_ID == 0:
-      debug("BASE_STATION_MODE")
-      exec(open('./Base.py').read())
-   else:
-      debug("ROVER_MODE")
-      exec(open('./Rover.py').read())
+   try:
+      if "data_entries" not in os.listdir("/"):
+         os.mkdir("/data_entries")
+      #input()
+      if DEVICE_ID == 0:
+         debug("BASE_STATION_MODE")
+         exec(open('./Base.py').read())
+      else:
+         debug("ROVER_MODE")
+         exec(open('./Rover.py').read())
+   except BaseException as error:
+      with open("error_log.txt", 'a') as file:
+         traceback.print_exception(type(error), error, error.__traceback__, None, file, False)
+      traceback.print_exception(type(error), error, error.__traceback__, None, None, False)
+   finally:
+      Device.shutdown()
